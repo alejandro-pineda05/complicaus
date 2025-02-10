eabfc6e800e0943636efe95fd8c662d513dac7bb
Jesús Vílchez <65301068+Jevimartinez@users.noreply.github.com>
2025-02-10 12:50:05 +0100

    Create Problema-G-bees-Jevi.py

---
+++ complicaus_3/Problema-G-bees-Jevi.py
@@ -0,0 +1,11 @@
+from bisect import bisect_right
+
+shops = int(input())
+prices = sorted([int(x) for x in input().split()])
+days = int(input())
+coins = [int(input()) for _ in range(days)]
+
+for day in range(days):
+    c = coins[day]
+    print(bisect_right(prices, c))
+    
